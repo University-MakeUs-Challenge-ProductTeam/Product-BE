c20b65a8584edf56e0b84dfcfa44252080d709ed
gohyeoncheol <huncher00@naver.com>
2025-02-13 13:09:19 +0900

    :recycle: refactor: 파일 경로,이름 수정

--- src/main/java/umc/product/global/config/security/auth/PrincipalDetailsService.java
+++ src/main/java/umc/product/global/config/security/auth/PrincipalDetailsService.java
@@ -1,7 +1,7 @@
 package umc.product.global.config.security.auth;
 
 import umc.product.domain.member.entity.Member;
-import umc.product.domain.member.service.MemberService;
+import umc.product.domain.member.serviceImpl.MemberServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -11,14 +11,14 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PrincipalDetailsService implements UserDetailsService {
 
-    private final MemberService detailMemberService;
+    private final MemberServiceImpl memberService;
 
     @Override
     public PrincipalDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         try { //username이 의미 하는 것은 엔티티의 id
              // 그래서 Long으로 변환
             Long userId = Long.parseLong(username); // Long으로 변환
-            Member memberEntity = detailMemberService.findById(userId); // 서비스에서 회원 조회
+            Member memberEntity = memberService.findById(userId); // 서비스에서 회원 조회
             return new PrincipalDetails(memberEntity); // PrincipalDetails 반환
         } catch (NumberFormatException e) {
             throw new UsernameNotFoundException("Invalid ID format", e); // 잘못된 ID 형식 예외 처리
