1dab04405f0da6977a5a2ef5a6fc7683f9f3a00e
gohyeoncheol <huncher00@naver.com>
2025-02-13 11:47:31 +0900

    :recycle: refactor: member 엔티티수정

--- src/main/java/umc/product/domain/member/entity/Member.java
+++ src/main/java/umc/product/domain/member/entity/Member.java
@@ -1,5 +1,10 @@
 package umc.product.domain.member.entity;
 
+import jakarta.validation.constraints.NotNull;
+import umc.product.domain.member.entity.enums.Gender;
+import umc.product.domain.member.entity.enums.LoginType;
+import umc.product.domain.member.entity.enums.Role;
+import umc.product.domain.member.entity.enums.University;
 import umc.product.global.common.enums.Status;
 import jakarta.persistence.*;
 import lombok.*;
@@ -7,6 +12,8 @@ import umc.product.global.common.base.BaseEntity;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseEntity {
 
@@ -14,51 +21,34 @@ public class Member extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Setter
-    private String name;
-
-    @Setter
-    private String profileImageUrl;
-
-    @Setter
-    private String birth;
-
-    @Setter
-    private Gender gender;
-
     @Enumerated(EnumType.STRING)
     private Role role;
 
     @Enumerated(EnumType.STRING)
-    private Status status;
+    private University university;
+
+    private String name;
+
+    private String nikeName;
+
+    private String email;
 
     @Enumerated(EnumType.STRING)
     private LoginType loginType;
 
-    private String clientId;
+    private String avatar_url;
 
-    // todo 편의상 일단 DB에 저장, 실제로는 저장하지 않게 해야 함
-    @Setter
-    private String refreshToken;
+    private String birth;
 
-    @OneToOne(fetch = FetchType.LAZY, mappedBy = "member", cascade = CascadeType.ALL)
-    private MemberLoginInfo memberLoginInfo;
+    private Gender gender;
 
-    @Builder
-    public Member(String name, String email, Role role, LoginType loginType, String clientId) {
-        this.name = name;
-        this.role = role;
-        this.loginType = loginType;
-        this.clientId = clientId;
-        this.status = Status.ACTIVE;
-    }
+    @Enumerated(EnumType.STRING)
+    private Status status;
+
+    private String password;
 
     public void changeRole(Role role) {
         this.role = role;
-    }
-
-    public void updateRefreshToken(String refreshToken) {
-        this.refreshToken = refreshToken;
     }
 
 
