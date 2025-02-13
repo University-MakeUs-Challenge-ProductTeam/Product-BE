32153e1cd2844ad3d9f069ee5882e9c3fffb886c
gohyeoncheol <huncher00@naver.com>
2025-02-13 13:08:57 +0900

    :recycle: refactor: 파일 경로,이름 수정

--- src/main/java/umc/product/global/config/security/SecurityConfig.java
+++ src/main/java/umc/product/global/config/security/SecurityConfig.java
@@ -1,7 +1,6 @@
 package umc.product.global.config.security;
 
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
-import umc.product.domain.member.entity.Role;
+import umc.product.domain.member.entity.enums.Role;
 import umc.product.global.config.security.auth.CustomAccessDeniedHandler;
 import umc.product.global.config.security.auth.PrincipalDetailsService;
 import umc.product.global.config.security.jwt.JwtAuthenticationFilter;
@@ -40,6 +39,7 @@ public class SecurityConfig {
                         .requestMatchers("/v3/api-docs/**", "/swagger-ui/**", "/swagger-ui.html", "/webjars/**").permitAll()
                         .requestMatchers("/s3/**").permitAll()
                         .requestMatchers("/members/auth/**").permitAll()
+                        .requestMatchers("/members/signup").permitAll()
                         .requestMatchers("/members/login").permitAll()
                         .requestMatchers("/admin/**").hasAuthority("ROLE_"+Role.ADMIN)
                         .requestMatchers("web/central-admin/**").hasAnyAuthority("ROLE_"+Role.CENTRAL_ADMIN, "ROLE_"+Role.ADMIN)
