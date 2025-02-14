0036fbecb94d7dafdce900a8a28e7f1686b97b2e
sshnote <springssh0504@naver.com>
2025-02-14 22:31:22 +0900

    ✨  feature: Project, Part 연관 관계 매핑
    ENUM 타입인 Part를 Project에서 List로 관리하기 위해 ProjectPart 엔티티를 생성하여 관리하도록 하였습니다.

--- src/main/java/umc/product/domain/project/entity/Project.java
+++ src/main/java/umc/product/domain/project/entity/Project.java
@@ -5,6 +5,7 @@ import lombok.*;
 import org.hibernate.annotations.DynamicInsert;
 import org.hibernate.annotations.DynamicUpdate;
 import umc.product.domain.member.entity.MemberProject;
+import umc.product.domain.member.entity.MemberProjectPart;
 import umc.product.domain.project.entity.mapping.ProjectTask;
 import umc.product.domain.project.entity.mapping.ProjectUniversity;
 import umc.product.domain.project.enums.Prize;
@@ -64,7 +65,8 @@ public class Project extends BaseEntity { // 프로젝트
     @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<MemberProject> memberProjects = new ArrayList<>();
 
-    // Part 연관 관계 매핑 추가 예정
+    @OneToMany(mappedBy = "project", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+    private List<ProjectPart> parts = new ArrayList<>();
 
     // Branch 연관 관계 매핑 추가 예정
 }
