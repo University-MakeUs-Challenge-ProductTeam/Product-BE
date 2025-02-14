eb5d40ac46dc04153dfbc2ab094b3285e86b0050
gohyeoncheol <huncher00@naver.com>
2025-02-14 11:55:24 +0900

    :recycle: refactor: 멤터 엔티티 avatar_url -> avatarUrl

--- src/main/java/umc/product/domain/member/entity/Member.java
+++ src/main/java/umc/product/domain/member/entity/Member.java
@@ -31,7 +31,7 @@ public class Member extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private LoginType loginType;
 
-    private String avatar_url;
+    private String avatarUrl;
 
     private String birth;
 
