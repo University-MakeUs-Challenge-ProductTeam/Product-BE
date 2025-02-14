d8f114ad3137f4c2d9a725c7cf64cad32cd98f0d
gohyeoncheol <huncher00@naver.com>
2025-02-14 11:57:43 +0900

    :recycle: refactor: suggestion 엔티티 comment, update function 추가

--- src/main/java/umc/product/domain/suggestion/entity/Suggestion.java
+++ src/main/java/umc/product/domain/suggestion/entity/Suggestion.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import umc.product.domain.member.entity.Member;
+import umc.product.domain.suggestion.dto.request.SuggestionRequest;
 import umc.product.domain.suggestion.entity.enums.SuggestionTarget;
 import umc.product.global.common.base.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Getter
@@ -36,4 +40,18 @@ public class Suggestion extends BaseEntity {
 
     @NotNull
     private boolean completedStatus;
+
+    @OneToMany(mappedBy = "suggestion", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<SuggestionComment> comments = new ArrayList<>();
+
+    public void updateSuggestion(SuggestionRequest request) {
+        this.anonymityStatus = request.isAnonymityStatus();
+        this.suggestionTarget = request.getSuggestionTarget();
+        this.content = request.getContent();
+        this.title = request.getTitle();
+    }
+
+    public void updateSuggestionStatus(boolean completedStatus) {
+        this.completedStatus = completedStatus;
+    }
 }
