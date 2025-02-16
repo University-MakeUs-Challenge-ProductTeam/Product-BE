ee01f6a8ed1da0e50013daf88588e4326ceec788
sshnote <springssh0504@naver.com>
2025-02-17 01:28:49 +0900

    ✨  feature: Project-Branch 추가 매핑

--- src/main/java/umc/product/domain/branch/entity/Branch.java
+++ src/main/java/umc/product/domain/branch/entity/Branch.java
@@ -2,7 +2,12 @@ package umc.product.domain.branch.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.SQLRestriction;
+import umc.product.domain.member.entity.MemberProject;
+import umc.product.domain.project.entity.Project;
 import umc.product.global.common.base.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Builder
@@ -24,4 +29,6 @@ public class Branch extends BaseEntity {
     @Column(nullable = false)
     private String name;
 
+    @OneToMany(mappedBy = "branch", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Project> projects = new ArrayList<>();
 }
