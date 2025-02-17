904bd7162a6911e7c117e277d09fd1fd48c1f7bf
sshnote <springssh0504@naver.com>
2025-02-17 16:20:34 +0900

    ✨  feature: 프로젝트 기간 예외처리 추가

--- src/main/java/umc/product/global/common/exception/code/status/ProjectErrorStatus.java
+++ src/main/java/umc/product/global/common/exception/code/status/ProjectErrorStatus.java
@@ -15,6 +15,7 @@ public enum ProjectErrorStatus implements BaseCodeInterface {
     MEMBER_PROJECT_PART_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER_PROJECT_PART404", "사용자가 참여한 프로젝트의 파트를 찾을 수 없습니다."),
     TASK_NOT_FOUND(HttpStatus.NOT_FOUND, "TASK404", "과제를 찾을 수 없습니다."),
     TASK_STATUS_NOT_FOUND(HttpStatus.NOT_FOUND, "TASK_STATUS_404", "과제 수행 여부를 찾을 수 없습니다."),
+    PROJECT_DURATION_NOT_FOUND(HttpStatus.NOT_FOUND, "PROJECT_DURATION_404", "프로젝트 기간을 찾을 수 없습니다."),
     ;
 
     private final HttpStatus httpStatus;
