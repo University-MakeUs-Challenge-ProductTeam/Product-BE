900078f515afe3ec7a29ea394d31df62e5753b9d
gohyeoncheol <huncher00@naver.com>
2025-02-23 21:08:01 +0900

    :recycle: refactor: mapper/converter 분리

--- src/main/java/umc/product/domain/member/adviser/admin/AdminAuthAdviser.java
+++ src/main/java/umc/product/domain/member/adviser/admin/AdminAuthAdviser.java
@@ -1,41 +1,39 @@
 package umc.product.domain.member.adviser.admin;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 import umc.product.domain.file.service.FileService;
+import umc.product.domain.member.converter.response.MemberConverter;
 import umc.product.domain.member.dto.request.admin.AdminLoginRequest;
 import umc.product.domain.member.dto.request.admin.AdminSignUpRequest;
-import umc.product.domain.member.dto.response.common.MemberGenerateTokenResponse;
-import umc.product.domain.member.dto.response.common.MemberIdResponse;
-import umc.product.domain.member.dto.response.common.MemberLoginResponse;
+import umc.product.domain.member.dto.response.member.MemberGenerateTokenResponse;
+import umc.product.domain.member.dto.response.member.MemberIdResponse;
+import umc.product.domain.member.dto.response.member.MemberLoginResponse;
 import umc.product.domain.member.entity.Member;
-import umc.product.domain.member.entity.MemberLoginInfo;
-import umc.product.domain.member.mapper.MemberInfoMapper;
-import umc.product.domain.member.mapper.MemberMapper;
 import umc.product.domain.member.service.admin.AdminAuthService;
+import umc.product.domain.member.service.admin.AdminMemberService;
 import umc.product.domain.member.service.common.MemberAuthService;
 import umc.product.domain.university.entity.University;
-import umc.product.domain.university.repository.UniversityRepository;
 import umc.product.domain.university.service.UniversityService;
 
 @Component
 @RequiredArgsConstructor
 public class AdminAuthAdviser {
     private final AdminAuthService adminAuthService;
+    private final AdminMemberService adminMemberService;
     private final MemberAuthService memberAuthService;
     private final UniversityService universityService;
     private final FileService fileService;
 
-    private final MemberMapper memberMapper;
+    private final MemberConverter memberConverter;
 
     public MemberIdResponse signUp(MultipartFile file, AdminSignUpRequest request){
         //FileCreateResponse fileCreateResponse = fileService.createFile("AVATAR-IMAGE", file);
         University university = universityService.findUniversity(request.getUniversity());
-        Member member = memberMapper.toAdminMember(request, "");
+        Member member = adminMemberService.toAdminMember(request, "");
         Member newMember = adminAuthService.signUp(member, request.getPassword(), university);
-        return memberMapper.toMemberIdResponse(newMember.getId());
+        return memberConverter.toMemberIdResponse(newMember.getId());
     }
 
     public MemberLoginResponse login(AdminLoginRequest request) {
