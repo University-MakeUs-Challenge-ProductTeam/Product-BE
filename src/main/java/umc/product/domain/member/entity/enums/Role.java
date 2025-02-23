566331ebbc81c39d112363eb84ee968026d967b9
gohyeoncheol <huncher00@naver.com>
2025-02-23 21:01:25 +0900

    :bulb: comm: 주석 추가

--- src/main/java/umc/product/domain/member/entity/enums/Role.java
+++ src/main/java/umc/product/domain/member/entity/enums/Role.java
@@ -8,10 +8,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Role {
-    ADMIN("관리자", 0),
-    CENTRAL_ADMIN("중앙 운영진", 1),
-    BRANCH_ADMIN("지부 운영진", 2),
-    UNIVERSITY_ADMIN("교내 운영진", 3),
+    ADMIN("관리자", 0),        //중앙 총괄, 부총괄
+    CENTRAL_ADMIN("중앙 운영진", 1),     //국장, 파트장
+    BRANCH_ADMIN("지부 운영진", 2),      //학교 회장, 부회장
+    UNIVERSITY_ADMIN("교내 운영진", 3),      //교내 파트장, 운영진
     CHALLENGER("일반 챌린저", 4),
     GUEST("비회원", 5);
 
