525efb1ab53e46384b1003155793481928e97fbb
gohyeoncheol <huncher00@naver.com>
2025-02-23 21:04:46 +0900

    :recycle: refactor: mapper/converter 분리

--- src/main/java/umc/product/domain/suggestion/mapper/SuggestionCommentMapper.java
+++ src/main/java/umc/product/domain/suggestion/mapper/SuggestionCommentMapper.java
@@ -44,20 +44,14 @@ public class SuggestionCommentMapper {
                 .build();
     }
 
-    public SuggestionCommentIdResponse toSuggestionCommentIdResponse(SuggestionComment suggestionComment) {
-        return SuggestionCommentIdResponse.builder()
-                .suggestionCommentId(suggestionComment.getId())
-                .build();
-    }
-
-    public List<SuggestionCommentsResponse> toPostDetailCommentResultDTO(List<SuggestionComment> comments, Long depth) {
+    public List<SuggestionCommentsResponse> toSuggestionDetailComment(List<SuggestionComment> comments, Long depth) {
 
         return comments.stream()
                 .filter(comment -> comment.getDepth().equals(depth))
                 .map(comment -> {
                     List<SuggestionCommentsResponse> childCommentsDTO =
                             comment.getChildComments() != null
-                                    ? toPostDetailCommentResultDTO(comment.getChildComments(), depth + 1)
+                                    ? toSuggestionDetailComment(comment.getChildComments(), depth + 1)
                                     : new ArrayList<>();
 
                     return SuggestionCommentsResponse.builder()
@@ -72,5 +66,4 @@ public class SuggestionCommentMapper {
                 })
                 .collect(Collectors.toList());
     }
-
 }
