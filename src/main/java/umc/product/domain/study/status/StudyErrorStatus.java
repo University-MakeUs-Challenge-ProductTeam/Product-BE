fe54567f41897f692912f537872625d75202cc69
sshnote <springssh0504@naver.com>
2025-03-02 20:17:58 +0900

    ✨  feature: 스터디 관련 예외 처리

---
+++ src/main/java/umc/product/domain/study/status/StudyErrorStatus.java
@@ -0,0 +1,32 @@
+package umc.product.domain.study.status;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import umc.product.global.common.exception.code.BaseCodeDto;
+import umc.product.global.common.exception.code.BaseCodeInterface;
+
+@Getter
+@AllArgsConstructor
+public enum StudyErrorStatus implements BaseCodeInterface {
+
+    STUDY_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_MEMBER404", "해당 스터디에 속한 사용자가 존재하지 않습니다."),
+    STUDY_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY404", "스터디가 존재하지 않습니다."),
+    STUDY_NAME_EMPTY(HttpStatus.BAD_REQUEST, "STUDY_NAME400", "스터디 이름은 비어있으면 안 됩니다."),
+    ;
+
+    private final HttpStatus httpStatus;
+    private final boolean isSuccess = false;
+    private final String code;
+    private final String message;
+
+    @Override
+    public BaseCodeDto getCode() {
+        return BaseCodeDto.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(isSuccess)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
