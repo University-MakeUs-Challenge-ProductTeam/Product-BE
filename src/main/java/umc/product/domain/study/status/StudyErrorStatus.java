d47d818665976656e1346f37125ac8d5c2f3fefd
sshnote <springssh0504@naver.com>
2025-03-02 22:09:53 +0900

    ✨  feature: 스터디 예외 처리 추가

--- src/main/java/umc/product/domain/study/status/StudyErrorStatus.java
+++ src/main/java/umc/product/domain/study/status/StudyErrorStatus.java
@@ -13,6 +13,8 @@ public enum StudyErrorStatus implements BaseCodeInterface {
     STUDY_MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_MEMBER404", "해당 스터디에 속한 사용자를 찾을 수 없습니다."),
     STUDY_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY404", "해당 스터디를 찾을 수 없습니다."),
     STUDY_NAME_EMPTY(HttpStatus.BAD_REQUEST, "STUDY_NAME400", "스터디 이름은 비어있으면 안 됩니다."),
+    STUDY_ATTENDANCE_NOT_FOUND(HttpStatus.NOT_FOUND, "STUDY_ATTENDANCE404", "해당 스터디 출석 정보를 찾을 수 없습니다."),
+    INVALID_ATTENDANCE_VALUE(HttpStatus.BAD_REQUEST, "STUDY_ATTENDANCE400", "유효하지 않은 참석 여부 체크값입니다. YES, NO 중에 보내주세요."),
     ;
 
     private final HttpStatus httpStatus;
