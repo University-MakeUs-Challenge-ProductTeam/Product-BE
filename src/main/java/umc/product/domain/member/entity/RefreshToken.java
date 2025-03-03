9f638c3d1dddf03dc6cf6ab030f9f99084946504
gohyeoncheol <huncher00@naver.com>
2025-03-03 20:40:12 +0900

    :sparkles: feat: redis crud->redisTemplete

--- src/main/java/umc/product/domain/member/entity/RefreshToken.java
+++ src/main/java/umc/product/domain/member/entity/RefreshToken.java
@@ -1,9 +1,6 @@
 package umc.product.domain.member.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Value;
+import lombok.*;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.index.Indexed;
@@ -11,9 +8,9 @@ import org.springframework.data.redis.core.index.Indexed;
 import java.util.UUID;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Builder
-@RedisHash(value = "jwtToken", timeToLive = 60*60*24*14) // 14일
 public class RefreshToken { //redis에 저장할 객체
 
     @Id
---
+++ src/main/java/umc/product/domain/member/service/member/MemberRefreshTokenService.java
@@ -0,0 +1,11 @@
+package umc.product.domain.member.service.member;
+
+import io.jsonwebtoken.Claims;
+import umc.product.domain.member.entity.RefreshToken;
+
+public interface MemberRefreshTokenService {
+    RefreshToken saveRefreshToken(String refreshToken, Long memberId);
+    void deleteRefreshToken(Long memberId);
+    boolean existRefreshToken(String refreshToken, Long memberId);
+    Claims getClaims(String refreshToken);
+}
