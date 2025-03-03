98a9e8da30439ab84c7871f5ac89032784896bae
gohyeoncheol <huncher00@naver.com>
2025-03-03 20:37:33 +0900

    :sparkles: feat: Role 최종 수정

--- src/main/java/umc/product/domain/member/entity/enums/Role.java
+++ src/main/java/umc/product/domain/member/entity/enums/Role.java
@@ -8,12 +8,13 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum Role {
-    ADMIN("관리자", 0),        //중앙 총괄, 부총괄
-    CENTRAL_ADMIN("중앙 운영진", 1),     //국장, 파트장
-    BRANCH_ADMIN("지부 운영진", 2),      //학교 회장, 부회장
-    UNIVERSITY_ADMIN("교내 운영진", 3),      //교내 파트장, 운영진
-    CHALLENGER("일반 챌린저", 4),
-    GUEST("비회원", 5);
+    ADMIN("관리자", 0),
+    CENTRAL_ADMIN("중앙 운영진", 1),
+    SCHOOL_ADMIN("학교 관리자", 2),     //web
+    BRANCH_STAFF("지부 운영진", 3),      //학교 회장, 부회장
+    UNIVERSITY_STAFF("교내 운영진", 4),      //교내 파트장, 운영진
+    CHALLENGER("일반 챌린저", 5),
+    GUEST("비회원", 6);
 
     private final String toKorean;
     private final int priority;
