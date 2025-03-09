d76a20b4220c3ca0d16b1e8bd98c1535221ec371
sshnote <springssh0504@naver.com>
2025-03-10 00:39:35 +0900

    ✨  feature: BranchUniversity 조회

--- src/main/java/umc/product/domain/branchUniversity/repository/BranchUniversityRepository.java
+++ src/main/java/umc/product/domain/branchUniversity/repository/BranchUniversityRepository.java
@@ -2,6 +2,11 @@ package umc.product.domain.branchUniversity.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import umc.product.domain.branchUniversity.entity.BranchUniversity;
+import umc.product.domain.university.entity.University;
+
+import java.util.Optional;
 
 public interface BranchUniversityRepository extends JpaRepository<BranchUniversity, Long> {
+
+    Optional<BranchUniversity> findByUniversity(University university);;
 }
--- src/main/java/umc/product/domain/branchUniversity/service/BranchUniversityService.java
+++ src/main/java/umc/product/domain/branchUniversity/service/BranchUniversityService.java
@@ -1,4 +1,9 @@
 package umc.product.domain.branchUniversity.service;
 
+import umc.product.domain.branchUniversity.entity.BranchUniversity;
+import umc.product.domain.university.entity.University;
+
 public interface BranchUniversityService {
+
+    BranchUniversity getBranchUniversity(University university);
 }
