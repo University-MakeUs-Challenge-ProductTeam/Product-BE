644fb43401bca52fa0f487ff4fd1fbe0a0c1f816
gohyeoncheol <huncher00@naver.com>
2025-03-13 11:12:46 +0900

    :sparkles: feat: 코드 인증시 교내/중앙 직책 분류

--- src/main/java/umc/product/domain/member/dto/response/member/code/MemberCodeVerifyResponse.java
+++ src/main/java/umc/product/domain/member/dto/response/member/code/MemberCodeVerifyResponse.java
@@ -3,15 +3,14 @@ package umc.product.domain.member.dto.response.member.code;
 import lombok.Builder;
 import umc.product.domain.member.entity.enums.Part;
 
-import java.util.List;
-
 @Builder
 public record MemberCodeVerifyResponse (
         Long memberId,
         String name,
         String nickName,
         Part part,
-        List<String> positionList
+        String universityPosition,
+        String centralPosition
 ){
 
 }
